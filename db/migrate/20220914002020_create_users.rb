77ae8a3bc39d1010cc3bbe18b87e32854f87e41b
toMilanesi <tomi.milanesi@gmail.com>
2022-09-14 09:28:23 -0300

    Add likes table

--- db/migrate/20220914002020_create_users.rb
+++ db/migrate/20220914002020_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :Name
-      t.string :Photo
-      t.string :Bio
-      t.decimal :PostCounter
+      t.text :Photo
+      t.text :Bio
+      t.integer :PostCounter
 
       t.timestamps
     end
