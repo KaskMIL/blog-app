d3b9561eba730cb3ec7b2bed0f3fc585e2fd0ad9
toMilanesi <tomi.milanesi@gmail.com>
2022-09-20 15:24:23 -0300

    Add test to post#show status response

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('index')
     end
   end
+
+  describe 'Show' do
+    before(:example) do
+      post = Post.create(title: 'Test post', text: 'Test post', author: @user)
+      get "/users/#{@id}/posts/#{post.id}"
+    end
+
+    it 'Should get a success response' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
