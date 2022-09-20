c28c9be7643c22213624a1b8f2c94c44fb30a6d0
toMilanesi <tomi.milanesi@gmail.com>
2022-09-20 13:39:16 -0300

    Add test to user#show route and template

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Users', type: :request do
+  describe 'Index' do
+    before(:example) { get root_path }
+
+    it 'Should get a success response' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Should render index template' do
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'Show' do
+    before(:example) { get '/users/show' }
+
+    it 'Should get a success response' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Should render show template' do
+      expect(response).to render_template('show')
+    end
+  end
+end
