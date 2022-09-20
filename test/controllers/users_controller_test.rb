f5ee179327393cf16dd829d0ea5abf2583122928
toMilanesi <tomi.milanesi@gmail.com>
2022-09-19 21:15:13 -0300

    Fix linters errors

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get id" do
+  test 'should get id' do
     get users_id_url
     assert_response :success
   end
 
-  test "should get posts" do
+  test 'should get posts' do
     get users_posts_url
     assert_response :success
   end
 
-  test "should get post_id" do
+  test 'should get post_id' do
     get users_post_id_url
     assert_response :success
   end
