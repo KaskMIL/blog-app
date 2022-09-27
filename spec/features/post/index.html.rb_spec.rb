05e649beb5e0e46faf8b4e5779c45d7f54218210
toMilanesi <tomi.milanesi@gmail.com>
2022-09-27 18:58:14 -0300

    Add tests to post#index

--- spec/features/post/index.html.rb_spec.rb
+++ spec/features/post/index.html.rb_spec.rb
@@ -1,51 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :system do
-  let!(:author) { User.where(name: 'Tom').first }
-  let!(:lilly) { User.where(name: 'Lilly').first }
-  let!(:post) { Post.where(title: 'Hello 3').first }
-  let!(:comment) { Comment.where(text: 'Hi Tom 4!').first }
-
-  before(:all) do
-    Rails.application.load_seed
-    driven_by(:selenium_chrome_headless)
+RSpec.describe 'user_posts_path' do
+  before(:each) do
+    @first_user = User.create(name: 'Tom')
+    @first_post = Post.create(title: 'Test post 1', text: 'Test post 1', author: @first_user)
+    @second_post = Post.create(title: 'Test post 2', text: 'Test post 2', author: @first_user)
+    @third_post = Post.create(title: 'Test post 3', text: 'Test post 3', author: @first_user)
+    @first_comment = Comment.create(text: 'First comment', author: @first_user, posts: @first_post)
   end
 
-  after(:all) do
-    Like.destroy_all
-    Comment.destroy_all
-    Post.destroy_all
-    User.destroy_all
+  it 'should show users picture' do
+    visit user_posts_path(@first_user)
+    expect(page).to have_css("img[src*='#{@first_user.photo}']")
   end
 
-  describe 'index page:' do
-    before(:example) do
-      visit user_posts_path(author.id)
-    end
+  it 'should show users name' do
+    visit user_posts_path(@first_user)
+    expect(page).to have_content(@first_user.name)
+  end
 
-    it 'shows the author\'s profile picture' do
-      image = page.all('img')
-      expect(image.size).to eq(1)
-    end
+  it 'should show number of posts' do
+    visit user_posts_path(@first_user)
+    expect(page).to have_content(@first_user.postCounter)
+  end
 
-    it 'shows the author\'s name' do
-      expect(page).to have_content(author.name)
-    end
+  it 'should show posts titles' do
+    visit user_posts_path(@first_user)
+    expect(page).to have_content('Test post 1')
+    expect(page).to have_content('Test post 2')
+    expect(page).to have_content('Test post 3')
+  end
 
-    it 'shows the number of posts' do
-      expect(page).to have_content(author.posts.size)
-    end
+  it 'should show first comment' do
+    visit user_posts_path(@first_user)
+    expect(page).to have_content('First comment')
+  end
 
-    # it 'shows post\'s title' do
-    #   expect(page).to have_content(post.title)
-    # end
+  it 'should show number of comments' do
+    visit user_posts_path(@first_user)
+    expect(page).to have_content(@first_post.commentsCounter)
+  end
 
-    # it 'shows some post\'s body' do
-    #   expect(page).to have_content(post.text[0..50])
-    # end
+  it 'should show number of likes' do
+    visit user_posts_path(@first_user)
+    expect(page).to have_content(@first_post.likesCounter)
+  end
 
-    # it 'shows the number of comments' do
-    #   expect(page).to have_content(post.comments.size)
-    # end
+  it 'should redirect to the post show page when click' do
+    visit user_posts_path(@first_user)
+    find("a[id=#{@first_post.id}]").click
+    expect(current_path).to eq(user_post_path(@first_user, @first_post))
   end
 end
