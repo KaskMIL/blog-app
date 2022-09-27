40f5d4d1badd9edf53157fbe83a1a6d4fc8df1ba
toMilanesi <tomi.milanesi@gmail.com>
2022-09-27 13:35:13 -0300

    Add postCounter test user#show

--- spec/features/user/index.html.rb_spec.rb
+++ spec/features/user/index.html.rb_spec.rb
@@ -1,31 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'users_path', type: :feature do
+  before(:each) do
+    @first_user = User.create(name: 'Tom')
+    @second_user = User.create(name: 'Lilly')
+  end
+
   describe 'root path' do
     it 'should display all users names' do
       visit root_path
-      expect(page).to have_content(User.first.name)
-      expect(page).to have_content(User.last.name)
+      expect(page).to have_content(@first_user.name)
+      expect(page).to have_content(@second_user.name)
     end
     it 'shows profile picture' do
       visit root_path
-      expect(page).to have_css("img[src*='#{User.first.photo}']")
-      expect(page).to have_css("img[src*='#{User.last.photo}']")
+      expect(page).to have_css("img[src*='#{@first_user.photo}']")
+      expect(page).to have_css("img[src*='#{@second_user.photo}']")
     end
     it 'Shows the number of posts each user has' do
       visit root_path
-      expect(page).to have_content(User.first.postCounter)
-      expect(page).to have_content(User.last.postCounter)
+      expect(page).to have_content(@first_user.postCounter)
+      expect(page).to have_content(@second_user.postCounter)
     end
     it 'Should redirect to first user_path' do
       visit root_path
       find("a[id=#{User.first.id}]").click
-      expect(current_path).to eq(user_path(User.first))
+      expect(current_path).to eq(user_path(@first_user))
     end
     it 'should redirects to last user_path' do
       visit root_path
       find("a[id=#{User.last.id}]").click
-      expect(current_path).to eq(user_path(User.last))
+      expect(current_path).to eq(user_path(@second_user))
     end
   end
 end
---
+++ spec/features/user/show.html.rb_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'user_path' do
+  before(:each) do
+    @first_user = User.create(name: 'Tom')
+    @second_user = User.create(name: 'Lilly')
+    @first_post = Post.create(title: 'Test post 1', text: 'Test post 1', author: @first_user)
+    @second_post = Post.create(title: 'Test post 2', text: 'Test post 2', author: @first_user)
+    @third_post = Post.create(title: 'Test post 3', text: 'Test post 3', author: @first_user)
+  end
+
+  describe 'user profile' do
+    it 'should see profile picture first user' do
+      visit user_path(@first_user)
+      expect(page).to have_css("img[src*='#{@first_user.photo}']")
+    end
+    it 'should see profile picture first user' do
+      visit user_path(@second_user)
+      expect(page).to have_css("img[src*='#{@second_user.photo}']")
+    end
+
+    it 'should see users name' do
+      visit user_path(@first_user)
+      expect(page).to have_content(@first_user.name)
+    end
+    it 'should see users name' do
+      visit user_path(@second_user)
+      expect(page).to have_content(@second_user.name)
+    end
+
+    it 'should see number of posts' do
+      visit user_path(@first_user)
+      expect(page).to have_content(@first_user.postCounter)
+    end
+    it 'should see number of posts' do
+      visit user_path(@second_user)
+      expect(page).to have_content(@second_user.postCounter)
+    end
+  end
+end
