0d6cb9d0bcc8caf6931f94f9c692f6451603db4c
toMilanesi <tomi.milanesi@gmail.com>
2022-09-28 20:23:35 -0300

    configure open_lettere gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { hots: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
