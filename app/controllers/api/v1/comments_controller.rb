19f274a00b408073c895d5685712c126c07445b5
toMilanesi <tomi.milanesi@gmail.com>
2022-09-30 12:59:42 -0300

    Create comment create to create comment from api call

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::CommentsController < ApplicationController
 
     render json: @post.comments
   end
+
+  def create
+    comment = Comment.new(text: params[:text], author: User.find(1), posts: Post.find(5))
+
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json :comment.errors, status: :unproccessable_entity
+    end
+  end
 end
